421093680d847c19875afae56f90711c30d986e1
Daniel Ramsay <daniel@dretzq.org.uk>
2014-05-22 00:00:24 +0100

    Fix long running MySQL connections

--- backend/record_results.php
+++ backend/record_results.php
@@ -1,5 +1,8 @@
 <?php
 
+# long running MySQL connections
+ini_set('mysqli.reconnect',1);
+
 include_once __DIR__ . "/../api/1.2/libs/DB.php";
 include_once __DIR__ . "/../api/1.2/libs/amqp.php";
 include_once __DIR__ . "/../api/1.2/libs/pki.php";
