f1587a847c1ff1df9e613c7f3ed39171595f1b73
Daniel Ramsay <daniel@dretzq.org.uk>
2014-06-10 18:44:31 +0100

    Queue stats cron script

---
+++ backend/queue_stats.php
@@ -0,0 +1,25 @@
+<?php
+
+include __DIR__ . "/../api/1.2/libs/DB.php";
+
+$conn = new APIDB($dbhost, $dbuser, $dbpass, $dbname);
+
+if (($fp = popen("/usr/sbin/rabbitmqctl list_queues","r")) == NULL) {
+	fwrite(stderr, "Unable to get Queue list\n");
+	exit(1);
+}
+
+$dt = date('Y-m-d H:I:S');
+while ($line = fgets($fp)) {
+	$results = array();
+	if (preg_match("/(url\.(.*)\.(ooni|org|public))\t(\d+)/", $line, &$matches)) {
+		$isp = $matches[2];
+		$type = $matches[3];
+		$count = $matches[4];
+
+		$conn->query("insert into queue_length(created, isp, type, length) values (?,?,?,?)",
+		array($dt, $isp, $type, $count));
+	}
+}
+$conn->commit();
+
