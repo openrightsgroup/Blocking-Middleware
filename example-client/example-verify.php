a55b1309d5bacd4d51e871b1e558a2532ccf20d6
Daniel Ramsay <daniel@dretzq.org.uk>
2014-07-29 23:26:52 +0100

    add example verify frontend page: openrightsgroup/cmp-issues#96

---
+++ example-client/example-verify.php
@@ -0,0 +1,70 @@
+<?php
+
+include_once "credentials.php";
+
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+?>
+<html>
+<head>
+<title>Web Client Test</title>
+</head>
+<body>
+<form method="POST" action="/example-client/example-verify.php">
+<div>
+TOKEN: <input type="input" name="token" /><br>
+</div>
+<input type="submit" />
+</form>
+</html>
+<?php
+} else {
+
+
+	$payload = array(
+		'email' => $USER,
+        'token' => $_POST['token'],
+		"date" => date("Y-m-d H:i:s")
+    );
+	// Sign it using $USER's secret
+	$payload['signature'] = sign($SECRET, $payload, array("token","date"));
+	$content = http_build_query($payload);
+
+
+	// build the request
+	$options = array(
+		'http' => array(
+			'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+			'method' => 'POST',
+			'content' => $content,
+		)
+	);
+
+	// send it
+	$ctx = stream_context_create($options);
+	$result = file_get_contents("$API/verify/email", false, $ctx);
+
+	// get the JSON data back from the api
+	$urldata = json_decode($result);
+
+	// now display
+?>
+<html>
+<head><title>Client Test</title>
+</head>
+<body>
+
+<h2>
+Submission result
+</h2>
+
+<div id="results">
+<?php
+var_dump($urldata);
+?>
+</div>
+
+</body>
+</html>
+<?php
+}
+?>
