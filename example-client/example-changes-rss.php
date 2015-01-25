f66b72e4032b7aee8f059481bc6ba651f7149ea9
Daniel Ramsay <daniel@dretzq.org.uk>
2015-01-25 09:48:17 +0000

    Example rss feed wrapper for changes feed - openrightsgroup/cmp-issues#127

---
+++ example-client/example-changes-rss.php
@@ -0,0 +1,52 @@
+<?php
+
+include "credentials.php";
+
+$dt = date("Y-m-d H:i:s");
+
+$args = array(
+	'email' => $USER,
+	'date' => $dt,
+	'signature' => createSignatureHash($dt, $SECRET )
+);
+$qs = http_build_query($args);
+
+// build the request
+$options = array(
+	'http' => array(
+		'method' => 'GET',
+		'ignore_errors' => '1',
+	)
+);
+
+// send it
+$ctx = stream_context_create($options);
+$result = file_get_contents("$API/recent/changes?$qs", false, $ctx);
+
+// get the JSON data back from the api
+$urldata = json_decode($result);
+
+header("Content-type: application/xml");
+
+print '<?xml version="1.0"?>
+<rss version="2.0">
+  <channel>
+    <title>Blocked.org.uk Recent Changes</title>
+	<link>http://localhost:8080/example-client/example-rss.php</link>
+	<language>en</language>
+	<ttl>10</ttl>
+	<generator>Example-client</generator>
+';
+
+foreach($urldata->results as $result) {
+$res = (array)$result;
+
+print "<item>\n";
+print "<title>{$res['url']} on {$res['network_name']}</title>\n";
+print "<link>https://www.blocked.org.uk/results?url={$res['url']}</link>\n";
+print "<description>{$res['url']} on {$res['network_name']} has changed from {$res['old_status']} to {$res['new_status']} ({$res['created']})</description>\n";
+print "<pubDate>{$res['created']}</pubDate>";
+print "</item>\n";
+}
+
+print "</channel></rss>";
