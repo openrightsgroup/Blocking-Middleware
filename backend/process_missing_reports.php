385a761dfb773f7278f1acec23842f216f4f405b
Daniel Ramsay <daniel@dretzq.org.uk>
2015-07-07 22:24:05 +0100

    missing report processor, add limit

--- backend/process_missing_reports.php
+++ backend/process_missing_reports.php
@@ -2,6 +2,11 @@
 include_once __DIR__ . "/../api/1.2/libs/DB.php";
 include_once __DIR__ . "/../api/1.2/libs/amqp.php";
 
+$limit = @$argv[1];
+if ($limit):
+    print "Limit: $limit\n";
+endif;
+
 $ch = amqp_connect();
 
 $ex = new AMQPExchange($ch);
@@ -10,7 +15,7 @@ $ex->setName('org.blocked');
 $conn = new APIDB($dbhost, $dbuser, $dbpass, $dbname);
 
 $rs = $conn->query("select report_entries.id, report_id from report_entries left join results on report_entry_id = report_entries.id
-where report_entry_id is null", array());
+where report_entry_id is null " . ($limit ? "limit $limit" : ""), array());
 
 while ($row = $rs->fetch_assoc()) {
 
