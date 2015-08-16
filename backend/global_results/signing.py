091912253acb4e86222a1058cb02f6984d5aee52
Daniel Ramsay <daniel@dretzq.org.uk>
2015-08-16 12:27:40 +0100

    retab

--- backend/global_results/signing.py
+++ backend/global_results/signing.py
@@ -4,8 +4,8 @@ import logging
 import datetime
 
 class RequestSigner(object):
-	def __init__(self, secret):
-		self.secret = secret
+    def __init__(self, secret):
+        self.secret = secret
 
     @staticmethod
     def encode(s):
@@ -15,14 +15,14 @@ class RequestSigner(object):
             return str(s)
         return s
 
-	def sign(self, *args):
-		msg = ':'.join([self.encode(x) for x in args])
-		logging.debug("Using signature string: %s", msg)
-		hm = hmac.new(self.secret, msg, hashlib.sha512)
-		return hm.hexdigest()
+    def sign(self, *args):
+        msg = ':'.join([self.encode(x) for x in args])
+        logging.debug("Using signature string: %s", msg)
+        hm = hmac.new(self.secret, msg, hashlib.sha512)
+        return hm.hexdigest()
 
-	def get_signature(self, args, keys):
-		return self.sign(*[args[x] for x in keys])
+    def get_signature(self, args, keys):
+        return self.sign(*[args[x] for x in keys])
 
-	def timestamp(self):
-		return datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+    def timestamp(self):
+        return datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
