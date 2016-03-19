a101b6cba2386d95c656ec78a1941d66a433ed87
Daniel Ramsay <daniel@dretzq.org.uk>
2016-03-19 23:55:57 +0000

    queue status

--- api/admin/queuestatus.php
+++ api/admin/queuestatus.php
@@ -2,6 +2,15 @@
 
 include "template.inc.php";
 
+/* Reads queue status from RabbitMQ, and displays stats in nice table.
+
+Requires a sudoers.d file like:
+
+Defaults:apache         !requiretty
+apache  ALL=(ALL:ALL)   NOPASSWD: /usr/sbin/rabbitmqctl -q list_queues name messages messages_unacknowledged consumers
+
+*/
+
 $fp = popen("/usr/bin/sudo /usr/sbin/rabbitmqctl -q list_queues name messages messages_unacknowledged consumers", "r");
 if (!$fp) {
     die ("Unable to get queue status");
@@ -10,6 +19,9 @@ if (!$fp) {
 $out = array();
 while ($line = fgets($fp)) {
     $parts = explode("\t", $line);
+    if (substr($parts[0],0,10) == "admin.view") {
+        continue;
+    }
     $out[] = array(
         'name' => $parts[0],
         'messages' => $parts[1],
