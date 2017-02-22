77e3994f9f632dd087e926d2fa0ffeab9a96d1bb
Daniel Ramsay <daniel@dretzq.org.uk>
2017-02-22 23:39:07 +0000

    add queue monitoring to importer

--- importers/import_uk_zonedb.php
+++ importers/import_uk_zonedb.php
@@ -9,6 +9,26 @@ function dbg($msg) {
     error_log($msg);
 }
 
+function check_queues() {
+    $cmd = "rabbitmqctl -q list_queues name messages";
+    $proc = popen($cmd, "r");
+    $maxlen = 0;
+    while ($line = fgets($proc)) {
+        list($name, $len) = str_split(trim($line), "\t");
+        $maxlen = $len > $maxlen ? $len : $maxlen;
+    }
+    pclose($proc);
+
+    return $maxlen;
+
+}
+
+// check queue lengths every $check_interval urls
+$check_interval = 100;
+
+// sleep if the queue is longer than queuelen_sleep
+$queuelen_sleep = 100;
+
 $fp = fopen($argv[1],"r");
 if (!$fp) {
     die ("Unable to open $argv[1]");
@@ -54,6 +74,10 @@ while ($line = fgetcsv($fp)) {
         $ex = new AMQPExchange($ch);
         $ex->setName('org.blocked');
         $ex->publish($msgbody, "check.fixed", AMQP_NOPARAM);
-        print "Queued\n";
+    }
+    if ($n % $check_interval == 0) {
+        while (check_queues() > $queuelen_sleep) {
+            sleep(30);
+        }
     }
 }
