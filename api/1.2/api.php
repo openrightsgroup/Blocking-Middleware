3427d871868bb797243053e3bfea820f33b3f9c8
Daniel Ramsay <daniel@dretzq.org.uk>
2017-07-29 18:56:52 +0100

    send email from site admin address, instead of user email

--- api/1.2/api.php
+++ api/1.2/api.php
@@ -1483,11 +1483,13 @@ $app->post('/ispreport/submit', function (Request $req) use ($app) {
 
 function sendISPReport($name, $email, $network, $url, $message, $report_type, $renderer) {
     $msg = new PHPMailer();
-    $msg->setFrom($email, $name . ' via Blocked.org.uk');
+    $msg->AddReplyTo($email, $name);
+    $msg->setFrom(SITE_EMAIL, $name . ' via Blocked.org.uk');
     $msg->Sender = SITE_EMAIL;
     $msg->addBCC(SITE_EMAIL);
     $msg->addAddress($network['admin_email'], $network['admin_name']);
     $msg->Subject = "Website blocking enquiry - " . $url;
+    #$msg->addCustomHeader("Auto-Submitted", "auto-generated");
     $msg->isHTML(false);
     $msg->CharSet = 'utf-8';
     $msg->Body = $renderer->render(
