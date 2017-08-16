87f4d35f0bc2a9c48f14eb4a0bca9a00be0c59a2
Daniel Ramsay <daniel@dretzq.org.uk>
2017-08-16 20:54:07 +0100

    bugfix

--- backend/process_unblocks.php
+++ backend/process_unblocks.php
@@ -16,11 +16,12 @@ $q = $conn->query("select isp_reports.id from
     isp_reports
     inner join url_status_changes usc on isp_reports.urlid = usc.urlid and isp_reports.network_name = usc.network_name
     where usc.new_status = 'ok' and isp_reports.unblocked = 0 and usc.created >= isp_reports.created 
-    and usc.created > now() - interval ?",
-    array($argv[1])
+    and usc.created >= (now() - interval '1 hour')",
+    array()
     );
 
 foreach($q as $row) {
+    echo "Updating report: " . $row['id'] . "\n";
     $conn->query("update isp_reports set unblocked = 1, last_updated = now() where id = ?",
         array($row['id'])
         );
