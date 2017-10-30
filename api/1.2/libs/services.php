a837a42ddcca6b1611c1bb6b7a0a89471c002186
Daniel Ramsay <daniel@dretzq.org.uk>
2017-10-30 20:30:12 +0000

    expose submitted timestamp; openrightsgroup/blocked-org-uk#186

--- api/1.2/libs/services.php
+++ api/1.2/libs/services.php
@@ -706,7 +706,7 @@ class ISPReportLoader {
         }
 
         $res = $this->conn->query("select 
-            url, network_name, fmtime(isp_reports.created) as created, unblocked
+            url, network_name, fmtime(isp_reports.created) as created, unblocked, fmtime(isp_reports.submitted) as submitted
             from isp_reports 
             inner join urls using(urlid)
             where report_type = ? $network_clause
