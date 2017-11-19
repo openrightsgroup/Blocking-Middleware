2fd0868b92a7a384215158044db1f918e82c905b
Simon StJG <Simon.StJG@gmail.com>
2017-11-19 10:17:52 +0000

    Make record_results.php more resilient to errors
    Prevent crashes on:
    * Bad probe UUID or invalid message signing
    * Message which isn't valid JSON
    
    Fix bug where message which isn't valid JSON won't be ack'd

--- backend/record_results.php
+++ backend/record_results.php
@@ -18,73 +18,71 @@ $q->setName('results');
 $conn = db_connect();
 
 $processor = new ResultProcessorService(
-	$conn,
-	new UrlLoader($conn),
-	new ProbeLoader($conn),
-	new IspLoader($conn)
-	);
+  $conn,
+  new UrlLoader($conn),
+  new ProbeLoader($conn),
+  new IspLoader($conn)
+);
 
 function process_result($msg, $queue) {
-	global $processor, $ex;
+  global $processor, $ex;
 
-	
-	$data = (array)json_decode($msg->getBody());
-	$queue->ack($msg->getDeliveryTag());
+  try {
 
-	error_log("Got result: {$data['probe_uuid']} {$data['date']} {$data['status']}");
+    $queue->ack($msg->getDeliveryTag());
+    $data = (array)json_decode($msg->getBody());
 
-	$probe = $processor->probe_loader->load($data['probe_uuid']);
-	if ($probe['enabled'] != 1) {
-		print "Probe not enabled.\n";
-		// drop message on floor
-		return true;
-	}
+    error_log("Got result: {$data['probe_uuid']} {$data['date']} {$data['status']}");
 
-	var_dump($data);
-	#workaround for unicode encoding bug
-	if (is_null($data['url'])) {
-		return true;
-	}
-	
+    $probe = $processor->probe_loader->load($data['probe_uuid']);
+    if ($probe['enabled'] != 1) {
+      print "Probe not enabled.\n";
+      // drop message on floor
+      return true;
+    }
 
-	Middleware::verifyUserMessage(
-		implode(":", array(
-			$data['probe_uuid'],
-			$data['url'],
-			$data['status'],
-			$data['date'],
-			$data['config']
-			)
-		),
-		$probe['secret'],
-		$data['signature']
-	);
-	
-	try {
-		$processor->process_result($data, $probe);
-	} catch (Exception $e) {
-		error_log("Caught exception: " . get_class($e));
-		error_log("Message was: " . $e->getMessage());
-	}
+    var_dump($data);
+    # workaround for unicode encoding bug
+    if (is_null($data['url'])) {
+      return true;
+    }
 
-	try {
+    Middleware::verifyUserMessage(
+      implode(":", array(
+        $data['probe_uuid'],
+        $data['url'],
+        $data['status'],
+        $data['date'],
+        $data['config']
+        )
+      ),
+      $probe['secret'],
+      $data['signature']
+    );
 
-		$forward = array(
-			'url' => $data['url'],
-			'network_name' => $data['network_name'],
-			'status' => $data['status'],
-			'blocktype' => $data['blocktype']
-		);
-		$ex->publish(json_encode($forward), 
-			$msg->getRoutingKey(), AMQP_NOPARAM);
+    try {
+      $processor->process_result($data, $probe);
+    } catch (Exception $e) {
+      error_log("processor->process_result failed.");
+      error_log("Caught exception: " . get_class($e));
+      error_log("Message was: " . $e->getMessage());
+    }
 
-	} catch(Exception $e) {
+    $forward = array(
+      'url' => $data['url'],
+      'network_name' => $data['network_name'],
+      'status' => $data['status'],
+      'blocktype' => $data['blocktype']
+    );
+    $ex->publish(json_encode($forward), $msg->getRoutingKey(), AMQP_NOPARAM);
 
-	}
+  } catch (Exception $e) {
+    error_log("process_result failed.");
+    error_log("Caught exception: " . get_class($e));
+    error_log("Message was: " . $e->getMessage());
+  }
 
-	return true;
-
+  return true;
 }
 
 $q->consume("process_result");
-
