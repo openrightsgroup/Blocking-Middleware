4987398883f95ef0efcd8010d799d65e3308b695
Daniel Ramsay <daniel@dretzq.org.uk>
2018-01-06 22:32:02 +0000

    fix networks list

--- api/1.2/api.php
+++ api/1.2/api.php
@@ -1018,7 +1018,7 @@ $app->get('/status/blocks/{region}', function(Request $req, $region) use ($app) 
                 'url' => $row['url'],
                 'first_blocked' => $row['first_blocked'],
                 'last_blocked' => $row['last_blocked'],
-                'network_name' => $row['network_name'],
+                'networks' => explode(",", substr($row['networks'], 1, -1)),
             );
         }
 
