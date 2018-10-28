12935dfd3b3689a73b4c92911cd453a4f3e14b36
Daniel Ramsay <daniel@dretzq.org.uk>
2018-10-28 14:44:18 +0000

    export fixes

--- backend/export_csv.php
+++ backend/export_csv.php
@@ -10,13 +10,13 @@ $conn = db_connect();
 $exclude_tags = "{uk-zone,com-zone,dotorg,dot-uk-zone,me-uk-zone,misc-uk-zone,org-uk-zone}";
 
 $result = $conn->query(
-    "select url, urls.inserted url_submitted, network_name, filter_level, uls.status, uls.created, http_status, config, uls.category, uls.blocktype 
+    "select url, urls.inserted url_submitted, network_name, uls.status, uls.created,  uls.category, uls.blocktype 
     from 
     url_latest_status uls
     inner join urls using (urlid)
     inner join isps on network_name = isps.name
-    where not tags <@ %s and regions && '{gb}' and urls.status = 'ok'
-    order by url
+    where not (urls.tags::varchar[] <@ ?::varchar[]) and regions && '{gb}'::varchar[] and urls.status = 'ok'
+    order by url, network_name, uls.created
     ", array($exclude_tags));
 
 if (count($argv) == 1) {
@@ -27,8 +27,8 @@ if (count($argv) == 1) {
 	$fp = fopen($filename,'w');
 }
 
-fputcsv($fp, array('# Latest results per URL/Network');
-fputcsv($fp, array('URL','URL Submission Timestamp','Network Name','Filter Level','Status','Result Timestamp','HTTP Status','Probe Config','Block Category','Block Type'));
+fputcsv($fp, array('# Latest results per URL/Network'));
+fputcsv($fp, array('URL','URL Submission Timestamp','Network Name','Status','Result Timestamp','Block Category','Block Type'));
 while ($row = $result->fetch(PDO::FETCH_NUM)) {
 	#print implode($row, "\t") . "\n";
 	fputcsv($fp, $row);
