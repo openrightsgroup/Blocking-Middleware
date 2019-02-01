31b992c153f74a2cdf4c45b2d5a2da9a885a3ba2
Daniel Ramsay <daniel@dretzq.org.uk>
2019-02-01 02:39:25 +0000

    sql grants for blockedadmin

---
+++ sql/blockedadmin.grants.sql
@@ -0,0 +1,6 @@
+GRANT UPDATE(tags) ON TABLE urls TO blockedadmin;
+GRANT UPDATE(unblocked) ON TABLE isp_reports TO blockedadmin;
+GRANT UPDATE(last_updated) ON TABLE isp_reports TO blockedadmin;
+GRANT UPDATE(status) ON TABLE isp_reports TO blockedadmin;
+GRANT UPDATE(resolved_email_id) ON TABLE isp_reports TO blockedadmin;
+
