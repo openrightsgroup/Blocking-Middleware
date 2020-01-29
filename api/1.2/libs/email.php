6d41a1000189a2242db8eb3ab5942eeedcee4187
Daniel Ramsay <daniel@dretzq.org.uk>
2020-01-29 21:34:41 +0000

    improved network name mapping

--- api/1.2/libs/email.php
+++ api/1.2/libs/email.php
@@ -45,9 +45,14 @@ function sendBBFCReport($mailname, $name, $email, $network, $url, $message, $pre
     #    <option value="O2">O2</option>
     #    <option value="Vodafone">Vodafone</option>
     #    <option value="Other">Other</option>
-    
+
+    $other = "";
     if ($network == "Three") {
         $network = "3"; 
+    }
+    if (!in_array($network, array("3","EE","O2","Vodafone") {
+        $other = $network;
+        $network = "Other";
     }
     
     
@@ -67,7 +72,7 @@ function sendBBFCReport($mailname, $name, $email, $network, $url, $message, $pre
            
     $data = array(
         "submitted[who_is_your_mobile_network_operator]" => $network,
-        "submitted[please_specify]" => "",  # other ISP
+        "submitted[please_specify]" => $other,  # other ISP
         "submitted[have_you_contacted_your_mobile_operator]" => "01", # yes "01"
         "submitted[what_was_your_mobile_operators_response_to_your_complaint]" => $previous, # multine
         "submitted[your_name]" => $name,
@@ -94,7 +99,7 @@ function sendBBFCReport($mailname, $name, $email, $network, $url, $message, $pre
         ->send();
     $status = $rsp->getStatus();
     $body = $rsp->getBody();
-    error_log("BBFC Submit $status $body");
+    error_log("BBFC Submit ($status) $body");
         
     return true;
 }
