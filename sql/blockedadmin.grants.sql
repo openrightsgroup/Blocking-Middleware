435a237a4b6ee063bec7fa14eb19fcd37468fbd4
Daniel Ramsay <daniel@dretzq.org.uk>
2021-02-11 23:21:53 +0000

    add column grant #106

--- sql/blockedadmin.grants.sql
+++ sql/blockedadmin.grants.sql
@@ -3,6 +3,7 @@ GRANT UPDATE(unblocked) ON TABLE isp_reports TO blockedadmin;
 GRANT UPDATE(last_updated) ON TABLE isp_reports TO blockedadmin;
 GRANT UPDATE(status) ON TABLE isp_reports TO blockedadmin;
 GRANT UPDATE(resolved_email_id) ON TABLE isp_reports TO blockedadmin;
+GRANT UPDATE(resolved_userid) ON isp_reports TO blockedadmin;
 
 GRANT SELECT,INSERT ON TABLE isp_report_comments TO blockedadmin;
 GRANT SELECT,UPDATE ON TABLE isp_report_comments_id_seq TO blockedadmin;
