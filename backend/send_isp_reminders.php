4621d098d4c07f6ba1bc3b2cd1d305d5a24d94cf
Daniel Ramsay <daniel@dretzq.org.uk>
2021-05-22 20:03:57 +0100

    send script updates #124

--- backend/send_isp_reminders.php
+++ backend/send_isp_reminders.php
@@ -12,6 +12,8 @@ include_once __DIR__ . "/silex/vendor/autoload.php";
 
 $conn = db_connect();
 $contactloader = new ContactLoader($conn);
+$urlloader = new UrlLoader($conn);
+$isploader = new ISPLoader($conn);
 
 $loader = new Twig_Loader_Filesystem(__DIR__ . "/../api/1.2/templates");
 $renderer = new Twig_Environment($loader, array(
@@ -33,7 +35,9 @@ if (isset($opts['h'])) {
 $q = $conn->query("select isp_reports.*, admin_name, admin_email
     from isp_reports
     inner join isps on network_name = isps.name
+    inner join contacts on contact_id = contacts.id
     where status = 'SENT' and created < now() - interval ('14 days')
+    and contact.verified = 1
     and (last_reminder is null or last_reminder < now() - interval('30 days'))",array());
     
 foreach ($q as $row) {    
@@ -43,10 +47,13 @@ foreach ($q as $row) {
         continue;
     }
 
-    sendIspReminder($row, $renderer);
+    $network = $isploader->load($row['network_name']);
+    $url = $urlloader->load($row['urlid']);
 
-    $q2 = $conn->query("update isp_reports set last_reminder = now() where id = ?", $row['id']);
+    sendIspReminder($row, $network, $url, $renderer);
 
+    $q2 = $conn->query("update isp_reports set reminder_count=reminder_count+1, last_reminder = now() where id = ?", $row['id']);
+    $conn->commit();
 }
 
 
