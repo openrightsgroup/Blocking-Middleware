0b19ab5eef67a9628b8d3e359733cb225958e34d
Daniel Ramsay <daniel@dretzq.org.uk>
2021-10-28 23:37:53 +0100

    add test script #156

---
+++ tests/report_submit_hold.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * SQL fixtures
+ *
+ * insert into categories(display_name, name, hold, namespace, created) 
+ *  values ('AdultXXX', 'adult-xxx', true, 'test', now());
+ * insert into urls(url, hash, source, status, url_type) 
+ *  values ('http://adultsite.example.com', md5('http://adultsite.example.com'), 'user', 'ok', 'SUBDOMAIN');
+ * insert into url_categories(urlid, category_id, created, enabled) 
+ *  values (2, 1, now(), true);
+ * insert into results(urlid, status, network_name, created) 
+ *  values (2, 'blocked', 'ExampleISP', now());
+ */
+
+
