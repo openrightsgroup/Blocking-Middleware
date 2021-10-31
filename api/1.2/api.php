9934b2c2102c18ebdb5659214fb3bd2d13e1a04e
Daniel Ramsay <daniel@dretzq.org.uk>
2021-10-31 18:17:09 +0000

    fix filter view of held reports #156

--- api/1.2/api.php
+++ api/1.2/api.php
@@ -1148,7 +1148,7 @@ $app->get('/status/ispreports', function (Request $req) use ($app) {
     $filter = array();
 
     foreach(array('open','sent','unresolved','reviewed','cancelled','featured','egregious','harmless','resubmit',
-        'accepted', 'rejected', 'not_accepted') as $ftype) {
+        'accepted', 'rejected', 'not_accepted','hold') as $ftype) {
         if ($req->get($ftype,null)) {
             $filter['state'] = $ftype;
             break;
