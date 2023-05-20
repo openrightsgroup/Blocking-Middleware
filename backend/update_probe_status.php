c9ee0619afb56bac1894fe461b842f516feb538b
Daniel Ramsay <daniel@dretzq.org.uk>
2023-05-20 20:12:51 +0100

    fix parameter checks

--- backend/update_probe_status.php
+++ backend/update_probe_status.php
@@ -6,7 +6,7 @@ include "$dir/../api/1.2/libs/services.php";
 
 $conn = db_connect();
 
-if (count($argv) != 2) {
+if (count($argv) != 3) {
     print "Usage: {$argv[0]} <probe_uuid> <status>\n";
     print "\tStatus: 0 (OK), 3 (Critical)\n";
     exit(1);
